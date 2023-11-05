52592bb47ab81034f45f15bf68ca56c72f6724bd
MKropiwnicki <mateusz.kropiwnicki@gmail.com>
2023-11-05 19:43:09 +0100

    button fix

--- Oddaj w dobre rece App/src/Components/Steps.jsx
+++ Oddaj w dobre rece App/src/Components/Steps.jsx
@@ -6,8 +6,17 @@ import BagIcon from '../assets/Icon-2.svg'
 import SearchIcon from '../assets/Icon-3.svg'
 import ArrowsIcon from '../assets/Icon-4.svg'
 import {StepsTile} from "./StepsTile.jsx";
+import {useNavigate} from "react-router-dom";
 
 export const Steps = () => {
+
+    const navigate = useNavigate();
+
+    const handleDonationForm = () => {
+        navigate('/oddaj-rzeczy')
+
+    };
+
     return(
         <section className='steps' id='steps'>
             <h1>Wystarczą 4 proste kroki</h1>
@@ -18,7 +27,9 @@ export const Steps = () => {
                 <StepsTile icon={SearchIcon} title='Zdecyduj komu chcesz pomóc' content='wybierz zaufane miejsce'/>
                 <StepsTile icon={ArrowsIcon} title='Zamów kuriera' content='kurier przyjedzie w dogodnym terminie'/>
             </div>
-            <NavButton name='oddaj rzeczy' classMod='btn bigBtn'/>
+            <NavButton navigate={handleDonationForm}
+                       name='oddaj rzeczy'
+                       classMod='btn bigBtn'/>
         </section>
     )
 }
